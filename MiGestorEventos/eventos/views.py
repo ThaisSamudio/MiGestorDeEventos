e28e0505c6a9870f597d8b3b38f53b7410e061c0
Thaís C. Samudio A. <samudioarauz@gmail.com>
2024-09-05 11:54:33 -0500

    Creacion de la vistas

--- MiGestorEventos/eventos/views.py
+++ MiGestorEventos/eventos/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import Evento
+
+class EventoListView(ListView):
+    model = Evento
+    template_name = 'eventos/evento_list.html'
+    context_object_name = 'eventos'
+
+
+from django.views.generic.edit import CreateView
+from .models import Evento
+from .forms import EventoForm
+
+class EventoCreateView(CreateView):
+    model = Evento
+    form_class = EventoForm
+    template_name = 'eventos/evento_form.html'
+    success_url = '/eventos/'
