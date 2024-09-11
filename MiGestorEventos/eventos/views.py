e69880dc805dc47f8208899074ad7ed2854182ea
Thaís C. Samudio A. <samudioarauz@gmail.com>
2024-09-11 11:54:52 -0500

    Creacion del ListView y CreateView para Organizadores
    Se crearon las clases listview y createview para los organizadores

--- MiGestorEventos/eventos/views.py
+++ MiGestorEventos/eventos/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Evento
 from .forms import EventoForm
 from  . import forms
+from django.views.generic import ListView, CreateView
+from .models import Organizador
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 
 def eventos(request):
     eventos = Evento.objects.all()
@@ -18,3 +22,16 @@ def crearEventos(request):
         form = EventoForm()
 
     return render(request, 'evento_form.html', {'form': form})
+
+class OrganizadorListView(ListView):
+    model = Organizador
+    template_name = 'organizador_list.html'
+    context_object_name = 'organizadores'
+
+class OrganizadorCreateView(CreateView):
+   model = Organizador
+   fields = '__all__'
+   success_url = '/organizadores/'
+   template_name = 'organizador_form.html'
+   success_message = 'Organizador creado correctamente.'
+
