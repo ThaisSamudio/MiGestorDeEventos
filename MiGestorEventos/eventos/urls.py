bf4211d9acd370774f975e0111a5f7f61d522a58
Thaís C. Samudio A. <samudioarauz@gmail.com>
2024-09-11 11:53:50 -0500

    Urls organizador-list y organizador-crear
    Se agregaron las urls de las vistas para ver y crear organizadores

--- MiGestorEventos/eventos/urls.py
+++ MiGestorEventos/eventos/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('eventos/', views.eventos, name='evento-list'),
     path('crearEvento/', views.crearEventos, name='evento_form'),
+    path('organizadores/', views.OrganizadorListView.as_view(), name='organizador-list'),
+    path('organizadores/crear/', views.OrganizadorCreateView.as_view(), name='organizador-crear'),
+    
 
 ]
