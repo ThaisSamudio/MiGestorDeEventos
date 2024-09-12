1592e8539cf428dca2755de5cc95d6ad55b173b4
Thaís C. Samudio A. <samudioarauz@gmail.com>
2024-09-12 07:49:19 -0500

    Se agregaron las url para login, registro y logout

--- MiGestorEventos/eventos/urls.py
+++ MiGestorEventos/eventos/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -7,6 +8,10 @@ urlpatterns = [
     path('crearEvento/', views.crearEventos, name='evento_form'),
     path('organizadores/', views.OrganizadorListView.as_view(), name='organizador-list'),
     path('organizadores/crear/', views.OrganizadorCreateView.as_view(), name='organizador-crear'),
+    path('login/', views.iniciarSesion, name='login'),
+    path('editar-evento/<int:id>/', views.editar_evento, name='editar-evento'),
+    path('registrarse/',views.registrarse, name='registrarse'),
+    path('logout/', views.cerrarSesion, name='logout'),
     
-
 ]
+
