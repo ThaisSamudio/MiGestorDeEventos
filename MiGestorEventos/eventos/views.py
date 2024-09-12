8cd1d7146ff028f85e84fa66e08d40e8b770cfb6
Thaís C. Samudio A. <samudioarauz@gmail.com>
2024-09-12 07:51:02 -0500

    Se definieron las vistas para iniciar sesion , editar evento registrase y cerrar sesion

--- MiGestorEventos/eventos/views.py
+++ MiGestorEventos/eventos/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, redirect 
-from .models import Evento
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Evento, Organizador
 from .forms import EventoForm
-from  . import forms
 from django.views.generic import ListView, CreateView
-from .models import Organizador
-from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth import logout
 
 def eventos(request):
     eventos = Evento.objects.all()
@@ -35,3 +36,52 @@ class OrganizadorCreateView(CreateView):
    template_name = 'organizador_form.html'
    success_message = 'Organizador creado correctamente.'
 
+
+def iniciarSesion(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            usuario = form.get_user()
+            login(request, usuario)
+            messages.success(request, 'Inicio de sesión exitoso.')
+            return redirect('evento-list')  # Redirigir a la lista de eventos después del login
+        else:
+            messages.error(request, 'Credenciales incorrectas. Inténtalo nuevamente.')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
+
+
+@login_required
+def editar_evento(request, id):
+    evento = get_object_or_404(Evento, id=id)
+    
+    if request.method == 'POST':
+        form = EventoForm(request.POST, instance=evento)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'El evento ha sido actualizado.')
+            return redirect('evento-list')
+    else:
+        form = EventoForm(instance=evento)
+
+    return render(request, 'editar_evento.html', {'form': form, 'evento': evento})
+
+
+def registrarse(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Autentica y logea al usuario después del registro
+            messages.success(request, 'Registro exitoso. ¡Bienvenido!')
+            return redirect('login')  # Redirige al usuario a la página principal
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'registrarse.html', {'form': form})
+
+def cerrarSesion(request):
+    logout(request)
+    return redirect('evento-list') 
